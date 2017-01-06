c98428034c97b38064f552bb88d4dba744e4dd7b
Naomi Kudren <N.Kudren@amaze.com>
2017-01-06 13:37:37 +0000

    adding jukebox

--- index.js
+++ index.js
@@ -28,3 +28,23 @@ function handleUpdate(){
 
 inputs.forEach(input => input.addEventListener("change", handleUpdate));
 inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
+
+
+function showDiv() {
+    var div = document.getElementById('menu');
+
+    if (div.style.display == 'none') {
+        div.style.display = 'block';
+    } else {
+        div.style.display = 'none';
+    }
+}
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+}
+
+window.addEventListener("keydown", playSound);
